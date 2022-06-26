9f516103285da71de99d1fbd82bf79e5e3f66914
Ramon Basto <42789151+ramonbcll@users.noreply.github.com>
2022-06-26 14:47:55 -0300

    Add files via upload

---
+++ e-commerce/js/btn.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector('.fa-eye')
+
+btn.addEventListener('click', function() {
+
+    let input = document.querySelector('#password');
+
+    if(input.getAttribute('type') == 'password') {
+        input.setAttribute('type', 'text');
+    } else {
+        input.setAttribute('type', 'password');
+    }
+
+});
